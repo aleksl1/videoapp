34d8b44e776f17ebc6b46491ea17e321662ffefd
aleksl1 <aleksander.slawek1@gmail.com>
2025-11-09 18:38:45 +0100

    chore: theme and text setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { router } from "expo-router";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
       {/* TODO: Add search bar and category lists */}
+      <Button title="Login" onPress={() => router.push("/login")} />
     </View>
   );
 }
@@ -12,8 +14,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 20,
