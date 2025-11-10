8ecdac3acba6a0b761a7bb181ae25de8756bd583
aleksl1 <aleksander.slawek1@gmail.com>
2025-11-10 15:27:59 +0100

    chore: query and youtube setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,22 @@
 import { Stack } from "expo-router";
+import { QueryProvider } from "@/src/providers/QueryProvider";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="settings"
-        options={{ headerShown: true, title: "Settings" }}
-      />
-      <Stack.Screen name="video/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <QueryProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="settings"
+          options={{ headerShown: true, title: "Settings" }}
+        />
+        <Stack.Screen name="video/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </QueryProvider>
   );
 }
