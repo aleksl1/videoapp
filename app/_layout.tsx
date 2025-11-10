c63627899e38d4ed53fff89600e0d1d3e812bf85
aleksl1 <aleksander.slawek1@gmail.com>
2025-11-10 23:11:48 +0100

    fix: rename login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout() {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen
           name="settings"
